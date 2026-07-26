169c258e6d0171fcf0c270b8be3fc23617eb5d45
Lucas Coutinho <mail@lucasacoutinho.com>
2026-07-26 11:58:16 -0300

    Release v1.2.0

--- tools/verify-native-runtime.php
+++ tools/verify-native-runtime.php
@@ -3,6 +3,7 @@
 use ClickHouse\Driver\ClientOptions;
 use ClickHouse\Driver\Column;
 use ClickHouse\Driver\Exception\ValidationException;
+use ClickHouse\Driver\Type;
 
 if (!extension_loaded('clickhouse')) {
     throw new RuntimeException('The native clickhouse extension is not loaded');
@@ -13,6 +14,24 @@ if (count($constructor->getParameters()) !== 20) {
     throw new RuntimeException(
         'The native clickhouse extension exposes obsolete ClientOptions arginfo'
     );
+}
+
+$nativeTypeIds = [
+    'Time' => 37,
+    'Time64' => 38,
+    'JSON' => 39,
+    'Bool' => 40,
+];
+
+foreach ($nativeTypeIds as $name => $expectedId) {
+    $case = constant(Type::class . "::{$name}");
+    $actualId = is_object($case) ? $case->value : $case;
+
+    if ($actualId !== $expectedId) {
+        throw new RuntimeException(
+            "The native clickhouse extension exposes an obsolete {$name} type ID"
+        );
+    }
 }
 
 $invalidValues = [
